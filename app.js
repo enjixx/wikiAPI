a979092bda44c20c0549617a01836a2f5f6f3e76
Enkhlen Khurelbaatar <enkhulek@gmail.com>
2020-08-10 19:14:38 -0700

    Created routes for specific articles and all articles

--- app.js
+++ app.js
@@ -27,6 +27,97 @@ const articleSchema = {
 // Init collection
 const Article = mongoose.model("article", articleSchema );
 
+////// Requests Targetting All Articles ///////
+app.route("/articles")
+    .get(function(req, res){
+        Article.find(function(err, results){
+            if(!err){
+                res.send(results);
+            }
+            
+        });
+    })
+    .post(function(req, res){
+    
+        const newArticle = new Article({
+            title: req.body.title,
+            content: req.body.content
+        });
+
+        newArticle.save(function(err){
+            if(!err){
+                res.send("Successfully added a new article");
+            }
+            else{
+                res.send(err);
+            }
+        });
+    })
+    .delete(function(req, res){
+        Article.deleteMany(function(err){
+            if(!err){
+                res.send("Successfully deleted all articles");
+            }
+            else{
+                res.send(err);
+            }
+        });
+    });
+
+    ////// Requests Targetting A Specific Articles ///////
+
+app.route("/articles/:articleName")
+    .get(function(req, res){
+        const articleTitle = req.params.articleName;
+        Article.findOne({title: articleTitle}, function(err, result){
+            if(result){
+                res.send(result);
+            } else {
+                res.send("No articles matching that title was found");
+            }
+        });
+    })
+    .put(function(req, res){
+        Article.updateOne(
+            {title: req.params.articleName},
+            {title: req.body.title, content: req.body.content},
+            {overwrite: true},
+            function(err){
+                if(!err){
+                    res.send("Successfully updated " + req.params.articleName);
+                }
+                else{ 
+                    res.send(err);
+                }
+            });
+    })
+    .patch(function(req, res){
+        Article.updateOne(
+            {title:req.params.articleName},
+            {$set: req.body},
+            function(err){
+                if(!err){
+                    res.send("Successfully updated " + req.params.articleName);
+                }
+                else{
+                    res.send(err);
+                }
+            });
+    })
+    .delete(function(req, res){
+        Article.deleteOne(
+            {title: req.params.articleName},
+            function(err){
+                if(!err){
+                    res.send("Succesfully deleted " + req.params.articleName);
+                }
+                else{
+                    res.send(err);
+                }
+            });
+    });
+    
+
 app.listen(3000, function(){
     console.log("Server started on port 3000");
 });
